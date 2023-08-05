385b40eb40e990eeb75cada5abc350f73ca041d6
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-08-05 09:36:31 +0700

    Add loading indicator

--- components/DevSelect.tsx
+++ components/DevSelect.tsx
@@ -1,6 +1,7 @@
 import React, { Dispatch, SetStateAction } from "react";
 import DevCard from "./DevCard";
 import { DevData } from "@/app/page";
+import { AiOutlineLoading3Quarters as LoadIcon } from "react-icons/ai";
 
 export default function DevSelect({
   setDevData,
@@ -21,12 +22,14 @@ export default function DevSelect({
     <div className="shadow-md rounded-md flex-1 p-4 bg-secondary min-h-[500px] flex flex-col">
       <div className="flex justify-between items-center mb-4">
         <div className="mb-2 text-2xl font-bold">Dev {number}</div>
-        <button
-          className="px-2 py-1 bg-white border-2 border-primary rounded-md text-primary font-bold hover:bg-dark"
-          onClick={() => resetDevData(number)}
-        >
-          Change
-        </button>
+        {devData.dev && (
+          <button
+            className="px-2 py-1 bg-white border-2 border-primary rounded-md text-primary font-bold hover:bg-dark"
+            onClick={() => resetDevData(number)}
+          >
+            Change
+          </button>
+        )}
       </div>
       <div className="flex-1 flex items-center justify-center w-full">
         {devData?.dev ? (
@@ -46,10 +49,16 @@ export default function DevSelect({
               className="px-4 py-2 text-xl flex-grow rounded-full border border-gray-300 focus:border-primary outline-none"
             />
             <button
-              className="bg-primary text-white px-4 py-2 text-xl rounded-full hover:opacity-90"
-              onClick={() => setDev(number, devData.searchValue)}
+              className="bg-primary text-white px-4 py-2 text-xl rounded-full hover:opacity-90 flex items-center gap-2"
+              onClick={() => {
+                setDevData((prev) => ({ ...prev, status: "LOADING" }));
+                setDev(number, devData.searchValue);
+              }}
             >
-              Choose
+              <span>Choose</span>
+              {devData.status === "LOADING" && (
+                <LoadIcon className="animate-spin" />
+              )}
             </button>
           </div>
         )}
