0930060ce8262588a3447e438f57fcfe00c43e1d
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-08-06 13:41:29 +0700

    Add logo in toast

--- components/Notification.tsx
+++ components/Notification.tsx
@@ -2,6 +2,8 @@ import React, { useContext } from "react";
 import { useNotification } from "@/contexts/Notifications";
 import { motion } from "framer-motion";
 import { RxCross2 as Cross } from "react-icons/rx";
+import logo from "@/assets/images/logo.svg";
+import Image from "next/image";
 
 const Notification = ({
   message,
@@ -16,23 +18,33 @@ const Notification = ({
     <motion.div
       initial={{ x: 50 }}
       animate={{ x: 0 }}
-      className="fixed top-8 right-8 bg-white shadow-md"
+      className="fixed top-8 right-8 bg-white shadow-lg"
     >
-      <div className="p-4 flex items-center justify-between gap-4">
-        <div>{message}</div>
-        <button className="hover:opacity-50" onClick={() => hideNotification()}>
-          <Cross />
-        </button>
+      <div className="flex">
+        <div className="py-2 px-4 bg-secondary flex items-center justify-center">
+          <Image src={logo} alt="logo" className="w-8" />
+        </div>
+        <div>
+          <div className="p-4 flex items-center justify-between gap-4">
+            <div>{message}</div>
+            <button
+              className="hover:opacity-50"
+              onClick={() => hideNotification()}
+            >
+              <Cross />
+            </button>
+          </div>
+          <div
+            className={`h-2 ${
+              status
+                ? status === "ERROR"
+                  ? "bg-red-500"
+                  : "bg-green-500"
+                : "bg-primary"
+            }`}
+          ></div>
+        </div>
       </div>
-      <div
-        className={`h-2 ${
-          status
-            ? status === "ERROR"
-              ? "bg-red-500"
-              : "bg-green-500"
-            : "bg-primary"
-        }`}
-      ></div>
     </motion.div>
   );
 };
