dbcef5b558354abb1f7fbfdca6942b728654ee28
Ansell Maximilian <ansellmaximilian@gmail.com>
2023-08-14 18:42:57 +0700

    isolate resolver

--- grafbase/resolvers/stats.ts
+++ grafbase/resolvers/stats.ts
@@ -1,13 +1,29 @@
-import getStats from "@/common/getStats";
-import { Dev } from "@/lib/services/devs";
+// import getStats from "@/common/getStats";
+// import { Dev } from "@/lib/services/devs";
+
+// export default function StatsResolver(
+//   { _id, username, name, numPosts, numReactions, numFollowers }: Dev,
+//   {}
+// ) {
+//   return getStats({
+//     numFollowers: numFollowers || 0,
+//     numPosts: numPosts || 0,
+//     numReactions: numReactions || 0,
+//   });
+// }
 
 export default function StatsResolver(
-  { _id, username, name, numPosts, numReactions, numFollowers }: Dev,
+  {
+    numPosts,
+    numReactions,
+    numFollowers,
+  }: { numPosts: number; numReactions: number; numFollowers: number },
   {}
 ) {
-  return getStats({
-    numFollowers: numFollowers || 0,
-    numPosts: numPosts || 0,
-    numReactions: numReactions || 0,
-  });
+  let totalPoints = numFollowers + numPosts + numReactions;
+  return {
+    strength: (totalPoints === 0 ? 1 / 3 : numPosts / totalPoints) * 100,
+    defense: (totalPoints === 0 ? 1 / 3 : numFollowers / totalPoints) * 100,
+    speed: (totalPoints === 0 ? 1 / 3 : numReactions / totalPoints) * 100,
+  };
 }
